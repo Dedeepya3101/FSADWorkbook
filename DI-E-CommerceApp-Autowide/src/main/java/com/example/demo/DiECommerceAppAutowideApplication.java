5a2b94910f42689fd5c5c7b05421770c2579b026
Dedeepya3101 <gonchaladedeepya@gmail.com>
2026-01-23 16:04:16 +0530

    20-01-2026

---
+++ DI-E-CommerceApp-Autowide/src/main/java/com/example/demo/DiECommerceAppAutowideApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiECommerceAppAutowideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiECommerceAppAutowideApplication.class, args);
+	}
+
+}
